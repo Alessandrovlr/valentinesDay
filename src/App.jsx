0e9c59afca74af1ce6d723d2d4533b27d8e85abb
Alessandro <aleluinisilva@gmail.com>
2026-06-12 16:20:45 -0300

    correção no import da rota

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { Rotas } from './router/rotas'
+import { Rotas } from './router/Rotas'
 
 
 function App() {
